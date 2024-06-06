1baf475a982862012357df78da224582e0b1c7cc
jessdebrito <debrito.herself@gmail.com>
2024-06-06 04:02:19 -0300

    findAll

--- src/services.ts
+++ src/services.ts
@@ -1,36 +1,19 @@
+import { prisma } from "../prisma/prisma.client";
 import { Manager, ManagerCreate } from "./interfaces";
 
 export class ManagerService {
-
-  private managerDatabase: Manager[] = [
-    { id: 3, name: "Patricia", email: "contato@patricia.com" },
-    { id: 1, name: "Bruna", email: "contato@bruna.com" },
-  ];
-
-  private generateNextId = () => {
-
-    
-    const lastManager = this.managerDatabase.sort((a, b) => a.id - b.id).at(-1);
-
-    return lastManager ? lastManager.id + 1 : 1 ;
-  };
-
-  public create = (payload: ManagerCreate) => {
-    const newManager = {
-      id: this.generateNextId(),
-      ...payload,
-    };
-    this.managerDatabase.push(newManager);
+  public create = async (payload: ManagerCreate) => {
+    const newManager = await prisma.manager.create({ data: payload });
 
     return newManager;
   };
 
-  public findAll = () => {
-    return this.managerDatabase;
+  public findAll = async () => {
+    //SELECT * FROM "Manager"
+    return await prisma.manager.findMany();
   };
 
-  public findOne = (managerId: number) => {
-    return this.managerDatabase.find((manager) => manager.id === managerId)
-  }
-
+ /*  public findOne = (managerId: number) => {
+    return this.managerDatabase.find((manager) => manager.id === managerId);
+  }; */
 }
